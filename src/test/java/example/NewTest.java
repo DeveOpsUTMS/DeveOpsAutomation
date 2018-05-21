995431b7de8ea3ab43f40d9292e12e01f3853ab8
Madhu Alishala <malishala@TLP.ads.valuelabs.net>
2018-05-21 15:15:57 +0530

    testing in maven project using jenkins

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,47 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	
+WebDriver driver;
+  @Test
+  public void testTitle() {
+	  	driver.get("https://www.valuelabs.com/");
+		String title = driver.getTitle();				 
+		AssertJUnit.assertTrue(title.contains("Global software,Product development and IT services company - ValueLabs")); 
+		
+		/*if(driver.getTitle().equals(title)){
+			System.out.println("Title verification successful");
+		}else{
+			System.out.println("title verification fail");
+		}*/
+  }
+  
+  @Test
+  public void testUrl(){
+	  driver.get("https://www.valuelabs.com/");
+	  String url = driver.getCurrentUrl();
+	  AssertJUnit.assertEquals(url, "https://www.valuelabs.com/");
+  }
+  
+ 
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver", "src/test/resources/org/automation/driver/chromedriver.exe");
+	  driver = new ChromeDriver();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
