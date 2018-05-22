e283ff9524dadbed04d44864d84e8010e8e6bc1c
Madhu Alishala <malishala@TLP.ads.valuelabs.net>
2018-05-22 10:50:52 +0530

    proving chrome options class

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -6,45 +6,61 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
-	
-WebDriver driver;
-  @Test
-  public void testTitle() {
-	  	driver.get("https://www.valuelabs.com/");
-		String title = driver.getTitle();				 
-		AssertJUnit.assertTrue(title.contains("Global software,Product development and IT services company - ValueLabs")); 
-		
-		/*if(driver.getTitle().equals(title)){
-			System.out.println("Title verification successful");
-		}else{
-			System.out.println("title verification fail");
-		}*/
-  }
-  
-  @Test
-  public void testUrl(){
-	  driver.get("https://www.valuelabs.com/");
-	  String url = driver.getCurrentUrl();
-	  AssertJUnit.assertEquals(url, "https://www.valuelabs.com/");
-  }
-  
- 
-  @BeforeTest
-  public void beforeTest() {
-	  
-	  
-	  //src/test/resources/org/automation/driver/chromedriver.exe
-	  System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-	  driver = new ChromeDriver();
-  }
 
-  @AfterTest
-  public void afterTest() {
-	  driver.quit();
-  }
+	WebDriver driver;
+	ChromeOptions chromeOptions = new ChromeOptions();
+
+	@Test
+	public void testTitle() {
+		driver.get("https://www.valuelabs.com/");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String title = driver.getTitle();
+		AssertJUnit
+				.assertTrue(title.contains("Global software,Product development and IT services company - ValueLabs"));
+
+		/*
+		 * if(driver.getTitle().equals(title)){ System.out.println(
+		 * "Title verification successful"); }else{ System.out.println(
+		 * "title verification fail"); }
+		 */
+	}
+
+	@Test
+	public void testUrl() {
+		driver.get("https://www.valuelabs.com/");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String url = driver.getCurrentUrl();
+		AssertJUnit.assertEquals(url, "https://www.valuelabs.com/");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+
+		// src/test/resources/org/automation/driver/chromedriver.exe
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		chromeOptions.addArguments("--headless");
+		chromeOptions.addArguments("--no-sandbox");
+		driver = new ChromeDriver(chromeOptions);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
 
 }
